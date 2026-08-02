bf7e8454a3b2ce73a9bac72b4bfdbe058de01a0b
manbtd0-cloud <manbtd0@gmail.com>
2026-08-03 00:25:23 +0500

    feat: expose combined Scissors Batch 01 media

--- apps/web/src/features/catalogue-media/index.ts
+++ apps/web/src/features/catalogue-media/index.ts
@@ -1,3 +1,4 @@
 export * from "./types";
 export * from "./validation";
-export * from "./scissors-batch-01";
+export { SCISSORS_BATCH_01_MEDIA } from "./scissors-batch-01-combined";
+export { SCISSORS_BATCH_01_OPERATING_MEDIA } from "./scissors-batch-01-operating";
