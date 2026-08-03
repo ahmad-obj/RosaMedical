894b6325c2636b323d6507dca34616c773a90797
manbtd0-cloud <manbtd0@gmail.com>
2026-08-03 19:21:54 +0500

    feat: export Chisels Batch 01 media

--- apps/web/src/features/catalogue-media/index.ts
+++ apps/web/src/features/catalogue-media/index.ts
@@ -2,3 +2,4 @@ export * from "./types";
 export * from "./validation";
 export { SCISSORS_BATCH_01_MEDIA } from "./scissors-batch-01-combined";
 export { SCISSORS_BATCH_01_OPERATING_MEDIA } from "./scissors-batch-01-operating";
+export { CHISELS_BATCH_01_MEDIA } from "./chisels-batch-01";
