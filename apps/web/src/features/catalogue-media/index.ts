d790389391b2c95711a4c302e35cb3c97802ee1a
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 00:50:04 +0500

    feat(web): expose approved Knives media

--- apps/web/src/features/catalogue-media/index.ts
+++ apps/web/src/features/catalogue-media/index.ts
@@ -4,4 +4,6 @@ export { SCISSORS_BATCH_01_MEDIA } from "./scissors-batch-01-combined";
 export { SCISSORS_BATCH_01_OPERATING_MEDIA } from "./scissors-batch-01-operating";
 export { CHISELS_BATCH_01_MEDIA } from "./chisels-batch-01";
 export { CUTTERS_BATCH_01_MEDIA } from "./cutters-batch-01";
-export { KNIVES_BATCH_01_MEDIA } from "./knives-batch-01";
+export {
+  KNIVES_BATCH_01_APPROVED_MEDIA as KNIVES_BATCH_01_MEDIA
+} from "./knives-batch-01-approved";
