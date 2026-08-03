323a167daba743df601c8fa0687f88fa11fed809
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 01:48:55 +0500

    feat: expose approved Punches Batch 01 media

--- apps/web/src/features/catalogue-media/index.ts
+++ apps/web/src/features/catalogue-media/index.ts
@@ -7,4 +7,6 @@ export { CUTTERS_BATCH_01_MEDIA } from "./cutters-batch-01";
 export {
   KNIVES_BATCH_01_APPROVED_MEDIA as KNIVES_BATCH_01_MEDIA
 } from "./knives-batch-01-approved";
-export { PUNCHES_BATCH_01_MEDIA } from "./punches-batch-01";
+export {
+  PUNCHES_BATCH_01_APPROVED_MEDIA as PUNCHES_BATCH_01_MEDIA
+} from "./punches-batch-01-approved";
