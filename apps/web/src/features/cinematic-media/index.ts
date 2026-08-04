6b67389f8d781d620530c2d69287fe53163de57e
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 21:08:00 +0500

    feat: export cinematic media manifest

---
+++ apps/web/src/features/cinematic-media/index.ts
@@ -0,0 +1,9 @@
+export {
+  CINEMATIC_MEDIA,
+  REQUIRED_CINEMATIC_SLOTS,
+  getCinematicMedia
+} from "./cinematic-media";
+export type {
+  CinematicMediaAsset,
+  CinematicMediaSlot
+} from "./cinematic-media";
