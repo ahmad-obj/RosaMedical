5c0e2f75c2700b558a90071cbf0ef8b10651ef42
manbtd0-cloud <manbtd0@gmail.com>
2026-08-22 12:47:25 -0700

    feat(web): export inquiry product media renderer

--- apps/web/src/features/inquiry/index.ts
+++ apps/web/src/features/inquiry/index.ts
@@ -1,6 +1,7 @@
 export { AddToInquiryButton } from "./add-to-inquiry-button";
 export { createInquiryItemFromProduct } from "./create-inquiry-item";
 export { InquiryCountLabel } from "./inquiry-count";
+export { InquiryLineMedia } from "./inquiry-line-media";
 export { InquiryPage } from "./inquiry-page";
 export { QuotationPage } from "./quotation-page";
 export {
