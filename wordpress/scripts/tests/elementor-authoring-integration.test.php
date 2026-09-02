7958613dca30f44152eb15409c827330f1090049
Muhammad Ahmad <manbtd0@gmail.com>
2026-09-02 20:41:00 +0500

    test(wordpress): require About and Contact Elementor widgets

--- wordpress/scripts/tests/elementor-authoring-integration.test.php
+++ wordpress/scripts/tests/elementor-authoring-integration.test.php
@@ -101,7 +101,7 @@ namespace {
     };
     WidgetRegistry::registerWidgets($widgetsManager);
 
-    $homeExpected = [
+    $expected = [
         'rosa-home-hero' => ['hero_eyebrow', 'hero_title', 'hero_body', 'hero_button', 'image'],
         'rosa-home-who' => ['who_eyebrow', 'who_title', 'who_body', 'who_button', 'stat_1_value', 'stat_1_label', 'stat_2_value', 'stat_2_label', 'stat_3_value', 'stat_3_label', 'image'],
         'rosa-home-featured' => ['featured_title', 'benefit_1_title', 'benefit_1_body', 'benefit_2_title', 'benefit_2_body', 'benefit_3_title', 'benefit_3_body'],
@@ -111,26 +111,35 @@ namespace {
         'rosa-home-why' => ['why_eyebrow', 'why_title', 'why_1_title', 'why_1_body', 'why_2_title', 'why_2_body', 'why_3_title', 'why_3_body', 'image'],
         'rosa-home-proof' => ['proof_1', 'proof_2', 'proof_3', 'proof_4', 'proof_5', 'proof_6'],
         'rosa-home-evidence' => ['evidence_eyebrow', 'evidence_title', 'evidence_body', 'evidence_1_title', 'evidence_1_body', 'evidence_2_title', 'evidence_2_body', 'evidence_3_title', 'evidence_3_body', 'image'],
+        'rosa-page-hero-about' => ['page_eyebrow', 'page_title', 'page_body'],
+        'rosa-about-who' => ['who_eyebrow', 'who_title', 'who_body', 'image'],
+        'rosa-about-stats' => ['stat_1_value', 'stat_1_label', 'stat_2_value', 'stat_2_label', 'stat_3_value', 'stat_3_label'],
+        'rosa-about-cards' => ['card_1_title', 'card_1_body', 'card_1_cta', 'card_2_title', 'card_2_body', 'card_2_cta', 'card_3_title', 'card_3_body', 'card_3_cta'],
+        'rosa-about-feature' => ['feature_eyebrow', 'feature_title', 'feature_body', 'image'],
+        'rosa-about-why' => ['why_title', 'why_1_title', 'why_1_body', 'why_2_title', 'why_2_body', 'why_3_title', 'why_3_body'],
+        'rosa-about-proof' => ['proof_1', 'proof_2', 'proof_3'],
+        'rosa-page-hero-contact' => ['page_eyebrow', 'page_title', 'page_body'],
+        'rosa-contact-layout' => ['location_label', 'phone_label', 'email_label', 'form_title', 'field_name', 'field_phone', 'field_subject', 'field_message', 'send_email'],
+        'rosa-contact-map' => ['map_eyebrow', 'map_button'],
     ];
 
-    $homeWidgets = [];
+    $registered = [];
     foreach ($widgetsManager->widgets as $widget) {
         if (! method_exists($widget, 'get_name')) {
             continue;
         }
         $name = $widget->get_name();
-        if (! isset($homeExpected[$name])) {
-            continue;
+        if (isset($expected[$name])) {
+            $registered[$name] = $widget;
         }
-        $homeWidgets[$name] = $widget;
     }
 
-    if (array_keys($homeWidgets) !== array_keys($homeExpected)) {
-        fail_test('Home Elementor widgets were not registered in the required order');
+    if (array_keys($registered) !== array_keys($expected)) {
+        fail_test('Rosa Elementor widgets were not registered in the required order');
     }
 
-    foreach ($homeExpected as $name => $expectedControls) {
-        $widget = $homeWidgets[$name];
+    foreach ($expected as $name => $expectedControls) {
+        $widget = $registered[$name];
         if ($widget->get_categories() !== ['rosa-medical']) {
             fail_test("{$name} is not registered in the Rosa Medical category");
         }
@@ -140,5 +149,14 @@ namespace {
         }
     }
 
-    fwrite(STDOUT, "PASS: Elementor integration and Home widget contracts\n");
+    $forbiddenContactControls = ['email', 'phone', 'address', 'address_ar', 'submit_endpoint', 'form_action'];
+    foreach (['rosa-contact-layout', 'rosa-contact-map'] as $name) {
+        $actualControls = array_keys($registered[$name]->controlsForTest());
+        $leaked = array_values(array_intersect($forbiddenContactControls, $actualControls));
+        if ($leaked !== []) {
+            fail_test("{$name} duplicates protected Business/backend controls: " . implode(',', $leaked));
+        }
+    }
+
+    fwrite(STDOUT, "PASS: Elementor integration and Rosa widget contracts\n");
 }
